a9a634af09a60abffa28bcab6d47e28a94e8aea1
Seyed Mahyar Askarinejad <askari.m1379@gmail.com>
2022-04-12 14:14:16 +0430

    1st commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SystemUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SdCardUsage();
+            while (true)
+            {
+                var metrics = client.GetSdCard();
+
+                Console.WriteLine("Total: " + metrics.Total);
+                Console.WriteLine("Used : " + metrics.Used);
+                Console.WriteLine("Free : " + metrics.Free);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ SdCardUsage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SystemUsage
+{
+    public class SdCard
+    {
+        public double Total;
+        public double Used;
+        public double Free;
+    }
+    class SdCardUsage
+    {
+        public SdCard GetSdCard()
+        {
+            var output = "";
+            var info = new ProcessStartInfo("df -Bm");
+            info.FileName = "/bin/bash";
+            info.Arguments = "-c \"df -Bm\"";
+            info.RedirectStandardOutput = true;
+
+            using (var process = Process.Start(info))
+            {
+                output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+            }
+
+            var lines = output.Split("\n").ToList();
+            lines.RemoveAt(0);
+            lines.RemoveAt(lines.Count - 1);
+
+            var metrics = new SdCard();
+
+            foreach (var item in lines)
+            {
+                metrics.Total += Convert.ToDouble(item.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1].Replace("M", ""));
+                metrics.Used += Convert.ToDouble(item.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2].Replace("M", ""));
+                metrics.Free += Convert.ToDouble(item.Split(" ", StringSplitOptions.RemoveEmptyEntries)[3].Replace("M", ""));
+            }
+            return metrics;
+        }
+    }
+}
